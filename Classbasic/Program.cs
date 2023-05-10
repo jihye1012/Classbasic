49ca0beeea65b0e920552b637728f908b4a90b76
Jihye Yang <s2127@e-mirim.hs.kr>
2023-05-10 12:55:34 +0900

    초기 커밋.

---
+++ Classbasic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc=new OtherClass
+            Console.WriteLine("Hello World!");
+        }
+        class ThirdClass
+        {
+
+        }
+    }
+    class SecondClass
+    {
+
+    }
+}
